97ca582274e800e1d4ea87e5a3f8b8454da61826
santinozaracho <40505201+santinozaracho@users.noreply.github.com>
2018-06-23 01:52:09 -0300

    Actualizando index

--- Seguimiento.php
+++ Seguimiento.php
@@ -1,16 +1,6 @@
-
-
 <?php
-    require_once(dirname(__FILE__) . '/wp-config.php');
-    $wp->init();
-    $wp->parse_request();
-    $wp->query_posts();
-    $wp->register_globals();
-    $wp->send_headers();
 
+echo "Su DNI es ";
+echo ($_POST);
 
-    <p> Su DNI es</p>
-    echo ($_POST);
-    
-    echo site_url();
 ?>
